2d2b58bf263fb45fe445807dd6233122c60c4e8b
vojtech.franek@livesystems.cz <franek.vojtech01@gmail.com>
2026-05-22 21:50:26 +0200

    chore: update proxy for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://klotilda-api-test.up.railway.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
